2771933d7363f26e4c39376958c8073af3776252
Andres Ramirez <andres200493@gmail.com>
2021-10-12 22:11:49 -0500

    Sesion 3 - Reto 2

---
+++ Sesion-03/Hilt/app/src/main/java/com/bedu/hilt/data/api/ApiHelper.kt
@@ -0,0 +1,13 @@
+package com.bedu.hilt.data.api
+
+import com.bedu.hilt.data.model.ResponsePeopleModel
+import com.bedu.hilt.data.model.ResponsePlanetModel
+import retrofit2.Response
+
+interface ApiHelper {
+
+    suspend fun getPeople(): Response<ResponsePeopleModel>
+
+    suspend fun getPlanets(): Response<ResponsePlanetModel>
+
+}
