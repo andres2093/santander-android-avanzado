c4fd751dd2d5eb9dd45c8154127f2804b56ee74f
Andres Ramirez <andres200493@gmail.com>
2021-10-26 21:33:11 -0500

    Sesion 7 - E3 - R2

--- Sesion-07/app/src/main/java/com/bedu/librerias/ShortbreadActivity.kt
+++ Sesion-07/app/src/main/java/com/bedu/librerias/ShortbreadActivity.kt
@@ -1,9 +1,16 @@
 package com.bedu.librerias
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.bedu.librerias.databinding.ActivityShortbreadBinding
+import com.bedu.librerias.utils.SUCCESS
+import com.bedu.librerias.utils.showToasty
+import com.google.android.material.snackbar.Snackbar
+import shortbread.Shortcut
 
+@Shortcut(id = "shortBread", icon = R.drawable.ic_add_light, shortLabel = "ShortBread")
 class ShortbreadActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityShortbreadBinding
@@ -14,5 +21,38 @@ class ShortbreadActivity : AppCompatActivity() {
         binding = ActivityShortbreadBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.btnSnackBar.setOnClickListener { showSnackBar() }
+        binding.btnDialog.setOnClickListener { showDialog() }
     }
+
+    @Shortcut(
+        id = "show_snack_bar",
+        icon = R.drawable.ic_clear_white_24dp,
+        shortLabel = "Show snackbar"
+    )
+    fun showSnackBar() {
+        Snackbar.make(binding.content, "show_snack_bar", Snackbar.LENGTH_SHORT).show()
+    }
+
+    @Shortcut(id = "show_dialog", icon = R.drawable.ic_check_white_24dp, shortLabel = "Show dialog")
+    fun showDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("show_dialog")
+            .setMessage(R.string.app_name)
+            .setNegativeButton("close") { dialog, _ -> dialog.dismiss() }
+            .setPositiveButton("Ok!") { dialog, _ ->
+                dialog.dismiss()
+                showToasty(
+                    this,
+                    SUCCESS,
+                    "Ok!",
+                    Toast.LENGTH_SHORT,
+                    true,
+                    null,
+                    null
+                )
+            }.create().show()
+    }
+
 }
